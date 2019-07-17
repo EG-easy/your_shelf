1a9581e0d6141ade39681d05c7da6b708f898ed5
nakaikento <okamoto2okamoto@gmail.com>
2019-07-18 00:57:08 +0900

    resolved login/logout problem

--- your_shelf/config/urls.py
+++ your_shelf/config/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('books/', include('books.urls'), name='books'),
     # path('users/', include('users.urls'), name='users'),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
